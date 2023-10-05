6673c9cc2a7a42b4449c8c6f1d46736ad54bc8c8
Haize <92583384+Wolfhaize@users.noreply.github.com>
2023-10-05 12:04:00 +0300

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,9 +5,9 @@ import { ReactNode } from "react";
 import Providers from "./providers";
 
 const title =
-  "Novel – Notion-style WYSIWYG editor with AI-powered autocompletions";
+  "Athena – A simple text editor with AI-powered autocompletions";
 const description =
-  "Novel is a Notion-style WYSIWYG editor with AI-powered autocompletions. Built with Tiptap, OpenAI, and Vercel AI SDK.";
+  "Athena is a simple teext editor with AI-powered autocompletions. Built with Tiptap, OpenAI, and Vercel AI SDK.";
 
 export const metadata: Metadata = {
   title,
